fe34f93229c03405a2f882406879cb1aad07120e
Iago Barreto <iago_floripa@hotmail.com>
2024-09-04 01:28:02 -0300

    1064 solution

--- problems/1064.js
+++ problems/1064.js
@@ -14,7 +14,24 @@
 // Exemplo Saída:
 // 4 valores positivos
 // 7.4
+
 var input = require('fs').readFileSync('./dev/stdin', 'utf8');
 var lines = input.split('\n');
 
-console.log(lines);
+var qtdPositivos = 0;
+var somaPositivos = 0;
+var resultado = 0;
+// Percorrer o array, identificando números positivos 
+lines.map((num) => {
+    if (num > 0) {
+        // é positivo. Adicione à varíavel contador de nmrs positivos
+        qtdPositivos = qtdPositivos + 1;
+        // é positivo. Adicione à soma total de números positivos
+        somaPositivos = parseFloat(num) + parseFloat(somaPositivos);
+    }
+
+});
+resultado = (somaPositivos/qtdPositivos).toFixed(2);
+
+console.log(qtdPositivos + ' valores positivos');
+console.log(resultado);
