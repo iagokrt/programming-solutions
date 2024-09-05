a3fd2664c479e48c6fd1dbeb2f7c2e79011a6b86
Iago Barreto <iago_floripa@hotmail.com>
2024-09-04 21:47:45 -0300

    1759 solution

---
+++ problems/1759.js
@@ -0,0 +1,28 @@
+// Papai Noel está brincando com seus duendes para entretê-los durante a véspera do Natal. A brincadeira consiste nos elfos escreverem números em pedaços de papel e colocarem no gorro do Papai Noel. Após todos terminarem de colocar os números Noel sorteia um papel e aquele número representa quantos "Ho" o Noel deve falar.
+
+// Seu trabalho é ajudar o Papai Noel montando um problema que mostre todos os "Ho" que ele deve falar dado o número sorteado.
+
+// Entrada
+// A entrada é composta por um único inteiro N (0 < N ≤ 106) representando quantos "Ho" serão falados por Noel.
+// 5
+
+// Saída
+// A saída é composta por todos "Ho" que Papai Noel deve falar separados por um espaço. Após o último "Ho" deve ser apresentado um "!" encerrando o programa.
+
+// Ho Ho Ho Ho Ho!
+
+var input = require('fs').readFileSync('./dev/stdin', 'utf8');
+var lines = input.split('\n').map((n) => parseInt(n)).shift();
+
+var result = '';
+for (let i = 1; i <= lines; i++) {
+    // console.log(i);
+    if (i == lines) {
+        result = result + 'Ho!'
+    } else {
+        result = result + 'Ho '
+    }
+    
+}
+
+console.log(result);
