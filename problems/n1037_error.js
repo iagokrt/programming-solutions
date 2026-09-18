91d385020822de15b83ad7fb18bb3dfad554fcc3
Iago Barreto <8128@tcesc.tc.br>
2026-09-18 18:25:30 -0300

    chato

---
+++ problems/n1037_error.js
@@ -0,0 +1,20 @@
+var input = require('fs').readFileSync('./dev/stdin', 'utf8');
+var lines = input.split('\n').map((n) => parseFloat(n));
+
+/**
+ * https://judge.beecrowd.com/pt/problems/view/1037
+ * 
+   25.00
+    Saida:
+
+   Intervalo [0,25]
+ */
+var intervals = {
+   [],
+   [],
+   []
+}
+
+if ()
+
+console.log(`Intervalo ${}`);
